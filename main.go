1433f047bc76de021a37ee7ac12721e240f1d783
Aoi1011 <ht.so.ik0102@gmail.com>
2023-02-08 08:39:30 +0700

    Update

--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html"
 	"log"
-	"net/http"
 )
 
+type Avenger struct {
+	RealName string `json:"real_name"`
+	HeroName string `json:"hero_name"`
+	Planet   string `json:"planet"`
+	Alive    bool   `json:"alive"`
+}
+
+func (a *Avenger) isAlive() {
+	a.Alive = true
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	avengers := []Avenger{
+		{
+			RealName: "Dr. Bruce Banner",
+			HeroName: "Hulk",
+			Planet:   "Midgard",
+		},
+		{
+			RealName: "Tony Stark",
+			HeroName: "Iron Man",
+			Planet:   "Midgard",
+		},
+		{
+			RealName: "Thor Odinson",
+			HeroName: "Thor",
+			Planet:   "Midgard",
+		},
+	}
 
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	avengers[1].isAlive()
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	jsonBytes, err := json.Marshal(avengers)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(jsonBytes))
 }
